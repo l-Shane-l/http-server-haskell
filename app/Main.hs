3928e0253e5dc19abfe1ec0ccef58dcd49788e3e
l-Shane-l <graainneog@proton.me>
2024-03-23 02:30:05 +0100

    pass 7th stage

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,41 @@
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import Control.Concurrent (forkIO)
-import Control.Monad (forever)
+import Control.Monad ()
+import Control.Monad.Reader
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BC
 import Network.Socket
 import Network.Socket.ByteString
+import System.Directory (doesFileExist)
+import System.Environment (getArgs)
 import System.IO (BufferMode (..), hSetBuffering, stdin, stdout)
 
-checkMsg :: BC.ByteString -> BC.ByteString
-checkMsg msg
-  | path == "/" = ok
-  | operation == "echo" = content $ BC.intercalate "/" $ drop 2 pathComponents
-  | operation == "user-agent" = content userAgent
-  | operation == "files" = ok
-  | otherwise = notFound
+-- Include other necessary imports here
+
+type Config = String -- Directory path
+
+type App = ReaderT Config IO
+
+checkMsg :: BC.ByteString -> App BC.ByteString
+checkMsg msg = do
+  directoy <- ask
+  if
+    | path == "/" -> return ok
+    | operation == "echo" -> return $ content $ BC.intercalate "/" $ drop 2 pathComponents
+    | operation == "user-agent" -> return $ content userAgent
+    | operation == "files" ->
+        case parseFileName path of
+          Just filename -> do
+            result <- readFileIfExists filename
+            case result of
+              Right contents -> return $ buildResponse "200 OK" "application/octet-stream" contents
+              Left errorMsg -> return $ notFound
+          Nothing -> return $ notFound
+    | otherwise -> return notFound
   where
     requestLines = BC.lines msg
     requestLine = head requestLines
@@ -26,6 +46,25 @@ checkMsg msg
     userAgent = BC.words (requestLines !! 2) !! 1
     notFound = buildResponse "404 Not Found" "" "Not Found"
     ok = buildResponse "200 OK" "" "Welcome!"
+
+-- Extracts the filename from the request path, if it matches the expected format.
+parseFileName :: BC.ByteString -> Maybe BC.ByteString
+parseFileName path =
+  case BC.split '/' path of
+    ["", "files", filename] -> Just filename
+    _ -> Nothing
+
+-- Attempts to read the file specified by the filename, if it exists in the directory.
+readFileIfExists :: BC.ByteString -> App (Either String BC.ByteString)
+readFileIfExists filename = do
+  directory <- ask
+  let filePath = directory ++ "/" ++ BC.unpack filename
+  fileExists <- liftIO $ doesFileExist filePath
+  if fileExists
+    then do
+      fileContents <- liftIO $ BS.readFile filePath
+      return $ Right fileContents
+    else return $ Left "File not found."
 
 buildResponse :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString
 buildResponse status contentType body =
@@ -40,10 +79,24 @@ buildResponse status contentType body =
       body
     ]
 
+handleConnection :: Socket -> SockAddr -> App ()
+handleConnection clientSocket clientAddr = do
+  liftIO $ BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
+  msg <- liftIO $ recv clientSocket 4000
+  liftIO $ BC.putStrLn msg
+  resp <- checkMsg msg -- Now correctly within the ReaderT monad
+  liftIO $ BC.putStrLn "final resp \n"
+  liftIO $ BC.putStrLn resp
+  liftIO $ sendAll clientSocket resp
+  liftIO $ close clientSocket
+
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stdin LineBuffering
+  args <- getArgs
+  let directory = head args -- Simplification for demonstration; consider using a parsing library for robustness
+
   -- You can use print statements as follows for debugging, they'll be visible when running tests.
   BC.putStrLn "Logs from your program will appear here"
 
@@ -64,13 +117,4 @@ main = do
   -- Accept connections and handle them forever
   forever $ do
     (clientSocket, clientAddr) <- accept serverSocket
-    forkIO $ do
-      BC.putStrLn $ "Accepted connection from " <> BC.pack (show clientAddr) <> "."
-      msg <- recv clientSocket 4000
-      BC.putStrLn msg
-      let resp = checkMsg msg
-      let finalResp = resp
-      putStrLn "final resp \n"
-      BC.putStrLn finalResp
-      sendAll clientSocket resp
-      close clientSocket
+    forkIO $ runReaderT (handleConnection clientSocket clientAddr) directory
